f478317492a5cbff6156c93add941219d4d921c3
Krish <krishm.imp@gmail.com>
2025-09-25 22:24:27 -0400

    third commit

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main (String[] args) {
-        System.out.println("non demo file");
-        System.out.println("some demo file");
+        System.out.println("de");
+        System.out.println("s 1");
     }
 }
