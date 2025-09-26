a11e341088d37181064979509d44fb4026e323ab
Krish <krishm.imp@gmail.com>
2025-09-25 22:25:34 -0400

    demo commit

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main (String[] args) {
-        System.out.println("non demo file");
-        System.out.println("some demo file");
+        System.out.println("demo smt");
+        System.out.println("mote");
     }
 }
